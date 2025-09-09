edddc9c6c8e68c0ee8f5fb26adeca40957a86182
ElioGlock <eliosilva12356@gmail.com>
2025-09-09 09:40:40 -0300

    new routes

--- rotas.py
+++ rotas.py
@@ -14,11 +14,26 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://www.google.com/maps")
 driver.maximize_window()
 
-routes = driver.find_element(By.ID,"hArJGc")
-routes.click()
+def open_routes():
+    routes = driver.find_element(By.ID,"hArJGc")
+    routes.click()
+    
+def put_places(enderecos):
+    start_place = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div[1]/div/input")
+    time.sleep(1)
+    start_place.send_keys("R. Antônio Cardoso Franco, 268 - Casa Branca, Santo André - SP, 09015-530")
+    finish_place = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[2]/div[1]/div/input")
+    finish_place.send_keys(enderecos)
+    finish_place.send_keys(Keys.ENTER)
 
 
  
-enderecos = "Praça Ismene Mendes, s/n - Centro, Uberlândia - MG, 38400-186"
+if __name__ == "__main__":
+    
+    enderecos = "Av. Pedro Álvares Cabral, s/n - Vila Mariana, São Paulo - SP, 04094-050"
+    # R. Giácomo Versolato - Nova Petrópolis, São Bernardo do Campo - SP, 09770-440
+    open_routes()
+    time.sleep(2)
+    put_places(enderecos)
 
 time.sleep(600)
