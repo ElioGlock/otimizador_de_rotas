0b7c0c9e42454877cb074c352793818e3b6e03d2
ElioGlock <eliosilva12356@gmail.com>
2025-09-10 14:01:55 -0300

    soulve problem if list > 4 elements

--- rotas.py
+++ rotas.py
@@ -22,28 +22,38 @@ def set_places():
     start_place = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div[1]/div/input")
     time.sleep(1)
     start_place.send_keys(startpoint)
-    finish_place = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[2]/div[1]/div/input")
-    finish_place.send_keys(enderecos[0])
-    finish_place.send_keys(Keys.ENTER)
+    second_place = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[2]/div[1]/div/input")
+    second_place.send_keys(enderecos[0])
+    second_place.send_keys(Keys.ENTER)
     
 def new_places():
-    for c in [1,2,3]:
-        new = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/button/div[2]")
-        new.click()
-        if c ==1:
-            f = 3
-        if c ==2:
-           f = 4
-        if c ==3:
-            f = 5
-        new_input = driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div[1]/div[{f}]/div[2]/div[1]/div/input")
-        new_input.send_keys(enderecos[c])
-        new_input.send_keys(Keys.ENTER)
-        time.sleep(2)
+    for c in list(range(len(enderecos))):
+        if c != 0:
+            new = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/button/div[2]")
+            new.click()
+            if c == 0:
+                pass
+            if c ==1:
+                f = 3
+            if c ==2:
+                f = 4
+            if c ==3:
+                f = 5
+            new_input = driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div[1]/div[{f}]/div[2]/div[1]/div/input")
+            new_input.send_keys(enderecos[c])
+            new_input.send_keys(Keys.ENTER)
+            time.sleep(2)
+        
+def return_time():
+    xpath = '//div[@id="section-directions-trip-0"]//div[contains(text(),"min")]'
+    wait = WebDriverWait(driver,timeout=3)
+    time_element = wait.until(EC.presence_of_element_located((By.XPATH,xpath)))
+    print(time_element.text)
 
  
 if __name__ == "__main__":
     startpoint = "R. Antônio Cardoso Franco, 268 - Casa Branca, Santo André - SP, 09015-530"
+    
     enderecos = ["Av. Pedro Álvares Cabral, s/n - Vila Mariana, São Paulo - SP, 04094-050",
                  "R. Manuel França dos Santos, 174 - Vila Sapopemba, São Paulo - SP, 03975-130",
                  "Praça Charles Miller, s/n - Pacaembu, São Paulo - SP, 01234-010",
@@ -54,5 +64,7 @@ if __name__ == "__main__":
     set_places()
     time.sleep(1)
     new_places()
+    time.sleep(1)
+    return_time()
  
 time.sleep(600)
